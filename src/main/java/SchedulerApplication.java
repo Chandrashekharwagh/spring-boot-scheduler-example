d73dba1c669aeba5513ff1cf1b3c6f39f3397899
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-15 16:14:53 +0530

    commits

--- src/main/java/SchedulerApplication.java
+++ src/main/java/SchedulerApplication.java
@@ -1,5 +1,3 @@
-package com.example.scheduler;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
